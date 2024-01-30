f29bd776fc7fac778db888516b9218084e1f0264
Angela <antunezangela89@gmail.com>
2024-01-30 08:08:59 +0100

    Two ways to execute the php script

--- database/MYSQLI/Connection.php
+++ database/MYSQLI/Connection.php
@@ -1,6 +1,9 @@
 <?php
 
-require "./.config";//I'm adding $password here to use it later as a parameter 
+// require "../../.config"; // If I do it this way. I have to type php Connection.php in the terminal
+
+require "./.config"; //If I do it this way. I have to type php .\database\MYSQLI\Connection.php in the terminal
+
 
 $server = "localhost";
 $database = "bookshop";
@@ -8,18 +11,23 @@ $username = "root";
 
 # ---------CONNECT-----------
 
-//a. Structure based on procedures
+//a. Structure based on procedures/methods/(algorithms, functions)
 
 $mysqliPr = mysqli_connect($server, $username, $password, $database);
 
-if(!$mysqliPr)
+if (!$mysqliPr) {
     die("Connection to database " . mysqli_connect_error() . "has failed");
-echo "ok";
+}
+echo "Connection established";
 
 //b. Structure based on OOP
 
 $mysqliOpp = new mysqli($server, $username, $password, $database);
 
-if($mysqliOpp->connect_errno)
+if ($mysqliOpp->connect_errno) {
     die("Connection to database {$mysqliOpp->connect_error} has failed");
-echo "ok";
+}
+$setNames = $mysqliOpp->prepare("SET NAMES 'utf8'");
+$setNames->execute();
+var_dump($setNames);
+echo "Connection established";
